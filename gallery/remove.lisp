8145ab7bc784b4c1f380d63a068b0a91e73afc81
pixel <pixel@c9dcec9a-3c5c-4c6f-ad4b-e69f8b72b15c>
2012-11-11 09:44:18 +0000

    Fixed for new phitamine.
    git-svn-id: file:///home/pixel/desk/code/repositories/shophope@8 c9dcec9a-3c5c-4c6f-ad4b-e69f8b72b15c

--- gallery/remove.lisp
+++ gallery/remove.lisp
@@ -1,21 +1,16 @@
 ;;;;; Copyright (c) 2012 Sven Michael Klose <pixel@copei.de>
 
-(defun image-removed (x)
-  (let old-action *action*
-    (= *navi-status* (lang en "The image has been removed."
-                           de "Das Bild wurde entfernt."))
-    (funcall old-action))
-  nil)
+(define-redirect-catcher (image-removed :status (lang en "The image has been removed."
+                                                      de "Das Bild wurde entfernt.")))
 
 (defun remove-image (x)
-  (with (id (required-component x.)
+  (with (id (number .x.)
          name (assoc-value 'name (find-image (list (cons 'id id)))))
     (alet (path-original (user-id) name)
       (& (file_exists !) (unlink !)))
     (alet (path-thumbnail (user-id) name)
       (& (file_exists !) (unlink !)))
     (delete-images (list (cons 'id id)))
-    (action-redirect :add 'image-removed)))
+    (action-redirect :remove 'remove-image :add 'image-removed)))
 
 (define-action remove-image  :group gallery)
-(define-action image-removed :group gallery)
