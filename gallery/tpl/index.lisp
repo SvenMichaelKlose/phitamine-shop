a86bfcc5a353137a9bff643ca514900a0725c486
pixel <pixel@c9dcec9a-3c5c-4c6f-ad4b-e69f8b72b15c>
2012-11-12 20:42:41 +0000

    No form if not logged in.
    git-svn-id: file:///home/pixel/desk/code/repositories/shophope@33 c9dcec9a-3c5c-4c6f-ad4b-e69f8b72b15c

--- gallery/tpl/index.lisp
+++ gallery/tpl/index.lisp
@@ -1,9 +1,10 @@
 (h1 ,(tpl-gallery-title) (br) ,(tpl-gallery-title-page))
 ,@(tpl-gallery-browser)
-(form :class "imageform"
-      :action ,(action-url :add 'update-images)
-      :method "post"
-  ,@(& (logged-in?)
-       `((input :class "imagesubmit" :type "submit" :value ,(lang de "Aktualisieren..."
-                                                                  en "Update..."))))
-  ,@(template-param 'images))
+,@(? (logged-in?)
+     `((form :class "imageform"
+             :action ,(action-url :add 'update-images)
+             :method "post"
+         (input :class "imagesubmit" :type "submit" :value ,(lang de "Aktualisieren..."
+                                                                  en "Update..."))
+         ,@(template-param 'images)))
+     (template-param 'images))
