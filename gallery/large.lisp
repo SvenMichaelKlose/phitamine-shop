65c9525d65b4c00eb560579e8f3efc67261b4ff1
pixel <pixel@c9dcec9a-3c5c-4c6f-ad4b-e69f8b72b15c>
2012-11-16 19:46:02 +0000

    Keep navigation in URL.
    git-svn-id: file:///home/pixel/desk/code/repositories/shophope@41 c9dcec9a-3c5c-4c6f-ad4b-e69f8b72b15c

--- gallery/large.lisp
+++ gallery/large.lisp
@@ -3,22 +3,21 @@
 (define-template tpl-gallery-image-large :path "gallery/tpl/image-large.lisp")
 
 (defun gallery-large (x)
-  (let old-components (copy-tree *components*)
-    (set-port
-      (with (images (gallery-find-images)
-             page (number .x.)
-             pagination (make-pagination :page page
-                                         :size 1
-                                         :total (get-image-count (gallery-image-selection)))
-             img (elt images (1- page)))
-        (= *gallery-pagination* pagination)
-        (= *gallery-page* (pagination-page pagination))
-        (= *gallery-images* images)
-        (= (page-title) (+ (assoc-value 'title img) " (" (assoc-value 'country img) ")"))
-        (tpl-gallery-image-large (+ `((image-src  . ,(original-src img))
-                                      (link-back  . ,(action-url old-components))
-                                      (pagination . ,pagination))
-                                    img)))))
+  (set-port
+    (with (images (gallery-find-images)
+           page (number .x.)
+           pagination (make-pagination :page page
+                                       :size 1
+                                       :total (get-image-count (gallery-image-selection)))
+           img (elt images (1- page)))
+      (= *gallery-pagination* pagination)
+      (= *gallery-page* (pagination-page pagination))
+      (= *gallery-images* images)
+      (= (page-title) (+ (assoc-value 'title img) " (" (assoc-value 'country img) ")"))
+      (tpl-gallery-image-large (+ `((image-src  . ,(original-src img))
+                                    (link-back  . ,(action-url :remove 'large))
+                                    (pagination . ,pagination))
+                                  img))))
   2)
 
 (define-action large :handler #'gallery-large)
