2b8eb05bab6b387499fee59af87e6af69cf32772
pixel <pixel@c9dcec9a-3c5c-4c6f-ad4b-e69f8b72b15c>
2012-12-02 12:40:04 +0000

    Stay in gallery after update.
    git-svn-id: file:///home/pixel/desk/code/repositories/shophope@52 c9dcec9a-3c5c-4c6f-ad4b-e69f8b72b15c

--- gallery/update.lisp
+++ gallery/update.lisp
@@ -3,9 +3,9 @@
 (defun gallery-update-images (x)
   (when (has-form?)
     (update-images (form-alists))
-    (action-redirect 'gallery-updated)))
+    (action-redirect :remove 'gallery-update-images :add 'gallery-updated)))
 
-(define-action gallery-update-images :group navi)
+(define-action gallery-update-images)
 
 (define-redirect-catcher (gallery-updated :status (lang de "Die Bilder wurden aktualisiert,"
                                                         en "The images have been updated.")))
