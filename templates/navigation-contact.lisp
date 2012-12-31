bf73e5dcde5bb60f74dfab02072ca6f582e3132b
pixel <pixel@c9dcec9a-3c5c-4c6f-ad4b-e69f8b72b15c>
2012-12-31 14:42:45 +0000

    Have placeholder for email address.
    git-svn-id: file:///home/pixel/desk/code/repositories/shophope@56 c9dcec9a-3c5c-4c6f-ad4b-e69f8b72b15c

--- templates/navigation-contact.lisp
+++ templates/navigation-contact.lisp
@@ -3,7 +3,7 @@
 (a :class "cancel" :href ,(navi-url) ,(lang de "zur&uuml;ck" en "back"))
 (form :action "" :method "post"
   (fieldset
-    (label :for "email" "E-Mail")(input :type "text" :value ,(form-value 'email) :name "email")
+    (label :for "email" "E-Mail")(input :type "email" :placeholder "yourname@yourdomain.net" :value ,(form-value 'email) :name "email")
     (label :for "text" "Text")(textarea :cols "40" :rows "12" :name "text" ,(form-value 'text))
     (input :type "submit" :value ,(lang de "Nachricht abschicken..."
                                         en "Send message..."))))
