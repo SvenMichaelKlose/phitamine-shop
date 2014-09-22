3f91435af01a86afd5de030f76f6875562f59d8b
Sven Michael Klose <pixel@copei.de>
2014-09-22 14:50:11 +0100

    .htaccess file is generated by phitamine.

--- makefiles/make.lisp
+++ makefiles/make.lisp
@@ -4,30 +4,31 @@
 (load "config-compile-time.lisp")
 
 (make-phitamine-project "Shop Hope Stamps"
-                        `("config-compile-time.lisp"
-                          "centralservices/pagination.lisp"
-                          "centralservices/user-management/login.lisp"
-                          "db-tables.lisp"
-                          "navigation.lisp"
-                          "page.lisp"
-                          "redirect-catcher.lisp"
-                          "flag.lisp"
-                          "gallery/templates/buy.lisp"
-                          "gallery/path.lisp"
-                          "gallery/image.lisp"
-                          "gallery/upload.lisp"
-                          "gallery/update.lisp"
-                          "gallery/remove.lisp"
-                          "gallery/browser.lisp"
-                          "gallery/title.lisp"
-                          "gallery/large.lisp"
-                          "gallery/index.lisp"
-                          "gallery/toplevel.lisp"
-                          "login.lisp"
-                          "contact.lisp"
-                          "imprint.lisp"
-                          "cart.lisp"
-                          ,@(& *have-css-upload?*
-                               '("css-upload.lisp"))
-                          "toplevel.lisp"))
+    :files `("config-compile-time.lisp"
+             "centralservices/pagination.lisp"
+             "centralservices/user-management/login.lisp"
+             "db-tables.lisp"
+             "navigation.lisp"
+             "page.lisp"
+             "redirect-catcher.lisp"
+             "flag.lisp"
+             "gallery/templates/buy.lisp"
+             "gallery/path.lisp"
+             "gallery/image.lisp"
+             "gallery/upload.lisp"
+             "gallery/update.lisp"
+             "gallery/remove.lisp"
+             "gallery/browser.lisp"
+             "gallery/title.lisp"
+             "gallery/large.lisp"
+             "gallery/index.lisp"
+             "gallery/toplevel.lisp"
+             "login.lisp"
+             "contact.lisp"
+             "imprint.lisp"
+             "cart.lisp"
+             ,@(& *have-css-upload?*
+                  '("css-upload.lisp"))
+             "toplevel.lisp")
+    :script-path "/shop")
 (quit)
