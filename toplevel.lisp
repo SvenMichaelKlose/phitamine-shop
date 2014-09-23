c74e70b007961e9640e3e5c5c544e8840502632e
Sven Michael Klose <pixel@copei.de>
2014-09-23 10:22:58 +0100

    phitamine doesn't connect to the database by default anymore.

--- toplevel.lisp
+++ toplevel.lisp
@@ -1,7 +1,8 @@
-;;;;; Copyright (c) 2012 Sven Michael Klose <pixel@copei.de>
+;;;;; Copyright (c) 2012,2014 Sven Michael Klose <pixel@copei.de>
 
 (include "config.php")
 
+(db-connect)
 (phitamine)
 
 (princ ,(format nil "<!DOCTYPE HTML>~%"))
