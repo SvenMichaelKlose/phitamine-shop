1ead325691f132008859647f5ff5793f8d3ad0b5
Sven Michael Klose <pixel@copei.de>
2014-09-26 17:34:13 +0100

    Tell that this is a demo shop.

--- templates/imprint.lisp
+++ templates/imprint.lisp
@@ -1,7 +1,5 @@
-(h2 "Payment")
-(p "I accept payment by PayPal only. Payment is expected immediately after purchase.")
-(h2 "Shipping")
-(p "I will ship it internationally. I will ship it as soon as possible after receiving of payment, with-in 2 working days. If you have any question do not hesitate to contact me by e-Mail fot further information.")
-(h2 "Feedback policy")
-(p "I will leave positive feedback to buyer as soon as buyer gives his.")
-(p "Check out my other items!")
+(h2 "Imprint")
+(p
+  "This is a demo of the "
+  (a :href "https://github.com/SvenMichaelKlose/phitamine-shop"
+     "phitamine shop") ".")
