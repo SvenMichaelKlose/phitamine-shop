5f783a52b34510f2d6daabff4a98222cfa7518de
Sven Michael Klose <pixel@copei.de>
2014-09-27 19:50:35 +0100

    Tell a little bit more and how to get a dedicated installation.

--- templates/imprint.lisp
+++ templates/imprint.lisp
@@ -1,5 +1,17 @@
 (h2 "Imprint")
 (p
   "This is a demo of the "
-  (a :href "https://github.com/SvenMichaelKlose/phitamine-shop"
+  (a :href "https://github.com/SvenMichaelKlose/phitamine-shop/"
      "phitamine shop") ".")
+(p
+  "It is based on "
+  (a :href "https://github.com/SvenMichaelKlose/phitamine/"
+     "phitamine")
+  ", which is a set of tools to write "
+  "PHP applications with the programming language "
+  (a :href "https://github.com/SvenMichaelKlose/tre/"
+     "tré")
+  ".")
+(p
+  "Should you want somebody to set up a taylored version of the "
+  "phitamine shop for you, please mail your request to user 'pixel' at hugbox.")
