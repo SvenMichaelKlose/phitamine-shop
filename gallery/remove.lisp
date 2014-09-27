3c140acda01614bb3a53a52d9580debe6f97fd67
Sven Michael Klose <pixel@copei.de>
2014-09-27 02:41:42 +0100

    Minor cleanup.

--- gallery/remove.lisp
+++ gallery/remove.lisp
@@ -1,15 +1,17 @@
-;;;;; Copyright (c) 2012 Sven Michael Klose <pixel@copei.de>
+;;;;; Copyright (c) 2012,2014 Sven Michael Klose <pixel@copei.de>
 
 (define-redirect-catcher (image-removed :status (lang en "The image has been removed."
                                                       de "Das Bild wurde entfernt.")))
 
+(defun unlink-if-exists (pathname)
+  (& (file_exists pathname)
+     (unlink pathname)))
+
 (defun remove-image (x)
-  (with (id (number .x.)
-         name (assoc-value 'name (find-image `(id . ,id))))
-    (alet (path-original (user-id) name)
-      (& (file_exists !) (unlink !)))
-    (alet (path-thumbnail (user-id) name)
-      (& (file_exists !) (unlink !)))
+  (with (id    (number .x.)
+         name  (assoc-value 'name (find-image `(id . ,id))))
+    (unlink-if-exists (path-original (user-id) name))
+    (unlink-if-exists (path-thumbnail (user-id) name))
     (delete-images (list (cons 'id id)))
     (action-redirect :remove 'remove-image :add 'image-removed)))
 
