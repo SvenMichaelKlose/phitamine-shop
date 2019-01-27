089a5abf63fc34f42acb4de471f06343f472c5fb
Sven Michael Klose <pixel@copei.de>
2019-01-27 19:56:43 +0100

    Make path for gallery images.

--- make.sh
+++ make.sh
@@ -4,3 +4,5 @@ mkdir compiled
 time tre makefiles/make.lisp && \
     cp -rv assets/* compiled && \
     mv -v compiled/htaccess compiled/.htaccess
+mkdir -p compiled/gallery-images
+chmod -R 777 compiled/gallery-images
