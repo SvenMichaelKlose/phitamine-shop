ceddd6b27db2dccf5dc059d3bdd286450bd21fcd
Sven Michael Klose <pixel@copei.de>
2019-01-28 07:08:53 +0100

    Now requires module sql-clause.

--- install-modules.sh
+++ install-modules.sh
@@ -3,5 +3,6 @@
 mkdir tre_modules
 git clone http://github.com/SvenMichaelKlose/tre-shared.git tre_modules/shared
 git clone http://github.com/SvenMichaelKlose/tre-php.git tre_modules/php
+git clone http://github.com/SvenMichaelKlose/tre-sql-clause.git tre_modules/sql-clause
 git clone http://github.com/SvenMichaelKlose/phitamine.git tre_modules/phitamine
 git clone http://github.com/SvenMichaelKlose/centralservices.git tre_modules/centralservices
