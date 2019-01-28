cda2e2a023dc0fcbd6e26b9e537f1893f86ceb99
Sven Michael Klose <pixel@copei.de>
2019-01-28 07:59:40 +0100

    Install module 'php-db-mysql'.

--- install-modules.sh
+++ install-modules.sh
@@ -2,7 +2,8 @@
 
 mkdir tre_modules
 git clone http://github.com/SvenMichaelKlose/tre-shared.git tre_modules/shared
-git clone http://github.com/SvenMichaelKlose/tre-php.git tre_modules/php
 git clone http://github.com/SvenMichaelKlose/tre-sql-clause.git tre_modules/sql-clause
+git clone http://github.com/SvenMichaelKlose/tre-php.git tre_modules/php
+git clone http://github.com/SvenMichaelKlose/tre-php-db-mysql.git tre_modules/php-db-mysql
 git clone http://github.com/SvenMichaelKlose/phitamine.git tre_modules/phitamine
 git clone http://github.com/SvenMichaelKlose/centralservices.git tre_modules/centralservices
