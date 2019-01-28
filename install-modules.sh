a801d14a01ae550e073bcd959693af10d7166ef8
Sven Michael Klose <pixel@copei.de>
2019-01-28 13:21:27 +0100

    Also install new module 'l10n'.

--- install-modules.sh
+++ install-modules.sh
@@ -2,6 +2,7 @@
 
 mkdir tre_modules
 git clone http://github.com/SvenMichaelKlose/tre-shared.git tre_modules/shared
+git clone http://github.com/SvenMichaelKlose/tre-l10n.git tre_modules/l10n
 git clone http://github.com/SvenMichaelKlose/tre-sql-clause.git tre_modules/sql-clause
 git clone http://github.com/SvenMichaelKlose/tre-php.git tre_modules/php
 git clone http://github.com/SvenMichaelKlose/tre-php-db-mysql.git tre_modules/php-db-mysql
