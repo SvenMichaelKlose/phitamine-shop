410854197dc608261b223bdc9b6a85e5a3ce7089
Sven Michael Klose <pixel@hugbox.org>
2019-02-20 07:15:09 +0100

    Properly exclude CSS upload.

--- src/templates/css-upload.lisp
+++ src/templates/css-upload.lisp
@@ -1,7 +1,8 @@
 (fn wg-css-upload ()
-  `((form :action ,(action-url :add 'css-upload)
-          :method "post"
-          :enctype "multipart/form-data"
-          :class "css_upload"
-      (input :type "file" :name "css[]")(br)
-      (input :type "submit" :value "Upload CSS…"))))
+  (when (| ,*have-css-upload?* nil) ; TODO: Optimizer bug in tré.
+    `((form :action ,(action-url :add 'css-upload)
+            :method "post"
+            :enctype "multipart/form-data"
+            :class "css_upload"
+        (input :type "file" :name "css[]")(br)
+        (input :type "submit" :value "Upload CSS…")))))
